3a88b3277835839760a21fb38a9b2f56c4a8fa36
Pete Whelpton <petewhelpton@aol.com>
2014-10-24 20:19:19 +0100

    Fraction class totally finished (hopefully!)

--- src/com/pwhelp01/Fraction.java
+++ src/com/pwhelp01/Fraction.java
@@ -22,6 +22,7 @@ public class Fraction {
     private int numerator;
     private int denominator;
 
+    
     /* Methods*/
     /**
      * Constructor for a new fraction.  Fraction will be simplified.
@@ -70,24 +71,6 @@ public class Fraction {
     }
 
     
-    /**
-     * Returns a string representation of the fraction in the format
-     * num/denom
-     * 
-     * @return fraction as a string num/denom
-     */
-    @Override
-    public String toString() {
-        
-    	/* Return a whole number if fraction can be simplified as such */
-    	if(this.getDenominator() == 1) {										// Check if fraction is a whole number
-    		return "" + this.getNumerator();									// If it is, return just the numerator
-    	}
-    	else {																	// If not, 
-    		return "" + getNumerator() + '/' + getDenominator();				// return the fraction in the format num/denom
-    	}
-    }
-    
     /* Getters and setters */
     /**
      * Get the fraction's numerator
@@ -123,6 +106,25 @@ public class Fraction {
     
     
     /**
+     * Returns a string representation of the fraction in the format
+     * num/denom
+     * 
+     * @return fraction as a string num/denom
+     */
+    @Override
+    public String toString() {
+        
+    	/* Return a whole number if fraction can be simplified as such */
+    	if(this.getDenominator() == 1) {										// Check if fraction is a whole number
+    		return "" + this.getNumerator();									// If it is, return just the numerator
+    	}
+    	else {																	// If not, 
+    		return "" + getNumerator() + '/' + getDenominator();				// return the fraction in the format num/denom
+    	}
+    }
+    
+    
+    /**
      * Compares two fractions for equality of numerator / denominator
      * 
      * @param o fraction to compare against
@@ -130,17 +132,24 @@ public class Fraction {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) return true;												// Check the object reference - if they are the same, the fractions must be equal!
+        if (o == null || getClass() != o.getClass()) return false;				// If the object is null, the fractions can't be equal!
 
-        Fraction fraction = (Fraction) o;
+        Fraction fraction = (Fraction) o;										// Instantiate a new Fraction from the object passed in
 
-        if (getDenominator() != fraction.getDenominator()) return false;
-        if (getNumerator() != fraction.getNumerator()) return false;
+        if (getDenominator() != fraction.getDenominator()) return false;		// Fractions are always simplified on creation, so if denominators do not match, nor do fractions
+        if (getNumerator() != fraction.getNumerator()) return false;			// Check numerators match.  If not, fractions are not equal
 
-        return true;
+        return true;															// If none of the above conditions are false, fractions must be equal
+        
     }
-
+    
+    
+    /**
+     * Override the default hashcode
+     * 
+     * Don't really understand what this does!
+     */
     @Override
     public int hashCode() {
         int result = numerator;
@@ -149,18 +158,33 @@ public class Fraction {
     }
 
     
+    /**
+     * Multiply two fractions
+     * 
+     * @param frac the fraction to multiply by
+     * @return new Fraction of the result
+     * @throws ArithmeticException if an overflow of int datatype would occur
+     */
     public Fraction multiply(Fraction frac) throws ArithmeticException{
 
-        long num = (long)this.getNumerator() * (long)frac.getNumerator();
-        long denom = (long)this.getDenominator() * (long)frac.getDenominator();
+        long num = (long)this.getNumerator() * (long)frac.getNumerator();		// Perform calculation and upcast new numerator
+        long denom = (long)this.getDenominator() * (long)frac.getDenominator();	// Perform calculation and upcast new denominator
         
         this.testRange(num);													// Test num and throw exception if out of range of int datatype
     	this.testRange(denom);													// Test denom and throw exception if out of range of int datatype
     	
-        return new Fraction((int)num, (int)denom);
+        return new Fraction((int)num, (int)denom);								// Return result as new Fraction
         
     }
     
+    
+    /**
+     * Add two fractions 
+     * 
+     * @param frac `the fraction to add to
+     * @return new Fraction of the result 
+     * @throws ArithmeticException if an overflow of int datatype would occur
+     */
     public Fraction add(Fraction frac) throws ArithmeticException{
     	
     	long num = ((long)this.getNumerator() * (long)frac.getDenominator())
@@ -174,6 +198,16 @@ public class Fraction {
     	
     }
 
+    
+    /**
+     * Find the greatest common denominator between two numbers
+     * 
+     * Keith wrote this - don't really know how it works...
+     * 
+     * @param a first number
+     * @param b second number
+     * @return greatest common denominator
+     */
     private int myGcd(int a, int b) {
         while (b != 0) {														
             int t = b;
@@ -183,6 +217,14 @@ public class Fraction {
         return a;
     }
 
+    
+    /**
+     * Subtract one fraction from another
+     * 
+     * @param frac the fraction to subtract
+     * @return new Fraction of the result
+     * @throws ArithmeticException if an overflow of the int datatype would occur
+     */
 	public Fraction subtract(Fraction frac) throws ArithmeticException{
 
 		long num = ((long)this.getNumerator() * (long)frac.getDenominator())
@@ -196,10 +238,17 @@ public class Fraction {
 	}
 	
 	
+	/**
+	 * Divide fraction by another
+	 * 
+	 * @param frac the fraction to divide by
+	 * @return new Fraction of the result
+	 * @throws ArithmeticException if an overflow of the int datatype would occur
+	 */
 	public Fraction divide(Fraction frac) throws ArithmeticException{
 		
-		long num = ((long)this.getNumerator() * (long)frac.getDenominator());
-		long denom = ((long)this.getDenominator() * (long)frac.getNumerator());
+		long num = ((long)this.getNumerator() * (long)frac.getDenominator());	// Calculate new numerator and upcast to long
+		long denom = ((long)this.getDenominator() * (long)frac.getNumerator());	// Calculate new denominator and upcast to long
 		
 		this.testRange(num);													// Test num and throw exception if out of range of int datatype
     	this.testRange(denom);													// Test denom and throw exception if out of range of int datatype
@@ -207,37 +256,57 @@ public class Fraction {
 		return new Fraction((int)num, (int)denom);								// Cast back to int datatype and return result
 	}
 	
-	public Fraction absValue() throws ArithmeticException{
+	
+	/**
+	 * Return the absolute (positive) value of a fraction
+	 * 
+	 * @return absolute value of fraction
+	 * @throws ArithmeticException 
+	 */
+	public Fraction absValue() {
 		
-		int num = this.getNumerator();
-		int denom = this.getDenominator();
+		int num = this.getNumerator();											// Get the numerator
+		int denom = this.getDenominator();										// Get the denominator
 		
-		if(num < 0) {
-			num *= -1;
+		if(num < 0) {															// If the numerator is negative,
+			num *= -1;															// flip it to be positive
 		}
-		if(denom < 0) {
-			denom *= -1;
+		if(denom < 0) {															// If the denominator is negative,
+			denom *= -1;														// flip it to be positive
 		}
 		
-		return new Fraction(num, denom);
+		return new Fraction(num, denom);										// Return result as new Fraction
 		
 	}
 	
+	
+	/**
+	 * Negate (flip) the sign of the fracion
+	 * 
+	 * @return negated Fraction
+	 */
 	public Fraction negate() {
 		
-		 int num = this.getNumerator() * -1;
-		 int denom = this.getDenominator();
+		 int num = this.getNumerator() * -1;									// As the constructor ensures only the numerator is signed,
+		 int denom = this.getDenominator();										// we only need to flip the numerator
 		 
-		 return new Fraction(num, denom);
+		 return new Fraction(num, denom);										// Return result as new Fraction
 
 		
 	}
 	
 	
-	public void testRange(long l) throws ArithmeticException {
+	/**
+	 * Helper method to check if the value of a long would 
+	 * overflow the int datatype
+	 * 
+	 * @param l value to check for an int overflow
+	 * @throws ArithmeticException if l would overflow int datatype
+	 */
+	private void testRange(long l) throws ArithmeticException {
 		
-		if((l < Integer.MIN_VALUE) || (l > Integer.MAX_VALUE)) {
-			throw new ArithmeticException("Overflow of type int");
+		if((l < Integer.MIN_VALUE) || (l > Integer.MAX_VALUE)) {				// If value is outside the range of the int datatype,
+			throw new ArithmeticException("Overflow of type int");				// throw and Arithmetic Exception
 		}	
 
 	}
